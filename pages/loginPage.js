6607a17176cf1a748a8200f163e6d4bbbde8adc8
Miguel Solano <miguel.abreu03@gmail.com>
2021-07-17 19:49:13 -0400

    chore: adding, moving and organizing files

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,8 +1,16 @@
-class loginPage extends SCREEN {
-    constructor (driver, selector) {
-        super(driver);
-        this.selector = selector;
+const {Builder, By, Key, until, WebDriverWait} = require("selenium-webdriver");
+
+module.exports = {
+    
+    login: async function (driver, data, page, successful = true) {
+        // web elements
+        let uidInput = await driver.findElement(By.id(page.uid_input))
+        let passwordInput = await driver.findElement(By.id(page.password_input));
+        let loginButton = await driver.findElement(By.name(page.login_button));
+        
+        // actions on web elements
+        await uidInput.sendKeys(data.email);
+        await passwordInput.sendKeys(successful? data.password:'wrongPassword');
+        await loginButton.click();
     }
-
-
 }
--- pages/screen.js
+++ pages/screen.js
@@ -1,6 +1,13 @@
-class SCREEN {
-    constructor (driver) {
-        this.driver = driver
-    }
+const {Builder, By, Key, until, WebDriverWait} = require("selenium-webdriver");
+
+module.exports = {
+    accessPage: async function  (driver, expectedTitle, url) {
     
+        // access login page
+        await driver.get(url);
+        
+        // assertion correct page 0_0" not ready yet
+        let title = await driver.getTitle();
+        console.log(title, expectedTitle);
+    },
 }

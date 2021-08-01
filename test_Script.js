866470fd1b9fd8b13f8d0219fa24e710649ba795
Miguel Solano <miguel.abreu03@gmail.com>
2021-08-01 19:10:35 -0400

    Feat: Adding the way to report the test

--- test_Script.js
+++ test_Script.js
@@ -1,6 +1,24 @@
+const {Builder, By, Key, until, WebDriverWait} = require("selenium-webdriver");
 const loginTest = require("./tests/loginTest");
 const facebook_page = require("./selectors/facebook");
 const twitter_page = require("./selectors/twitter");
+var { assert } = require("chai");
 
-loginTest.succesfulLogin('firefox', twitter_page, 'twitter');
-loginTest.succesfulLogin('firefox', facebook_page, 'facebook');
+describe('Facebook', function() {
+    describe('Log In',  function() {
+        it('Should be able to log in successfully to facebook', async function() {
+            await loginTest.succesfulLogin('firefox', facebook_page, 'facebook');
+        })
+    });
+});
+
+describe('Twitter', function() {
+    describe('Log In',  function() {
+        it('Should be able to log in successfully to twitter', async function() {
+            await loginTest.succesfulLogin('firefox', twitter_page, 'twitter');
+        })
+    });
+});
+
+//loginTest.succesfulLogin('firefox', twitter_page, 'twitter');
+//loginTest.succesfulLogin('firefox', facebook_page, 'facebook');

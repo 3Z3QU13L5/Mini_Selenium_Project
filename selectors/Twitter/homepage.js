4382a601e9b7409ca90d96170a1a317c2a7f7c38
Miguel Solano <miguel.abreu03@gmail.com>
2021-12-17 19:41:52 -0400

    Fix: updateing twitter selectors

--- selectors/Twitter/homepage.js
+++ selectors/Twitter/homepage.js
@@ -2,5 +2,9 @@ module.exports = {
     
     page_title: ['Twitter', 'home / twitter'],
     waiting_element: 'div[data-testid="primaryColumn"]',
+    input_placeholder: 'div[data-testid="tweetTextarea_0"]',
+    text_input: 'div[data-testid="tweetTextarea_0"]',
+    post_button: 'div[data-testid="tweetButtonInline"]',
+    //post: "div.c1et5uql",
 
 }
--- selectors/Twitter/loginpage.js
+++ selectors/Twitter/loginpage.js
@@ -1,13 +1,12 @@
 module.exports = {
     
     page_title: ['Twitter', 'Iniciar sesión en Twitter / Twitter'],
-    uid_input: 'input[name="session[username_or_email]"]', //Attributes
-    password_input: 'input[name="session[password]"]', //Attributes
+    uid_input: 'input[autocomplete="username"]', //Attributes
+    password_input: 'input[name="password"]', //Attributes
     login_button: 'div[data-testid="LoginForm_Login_Button"]', //Attributes
     waiting_element: 'input[name="session[username_or_email]"]', 
     header: '',
     create_button: 'div[aria-label="Create"]',
     warning_message: 'div[role="alert"]',
     warning_messageText: 'El nombre de usuario y la contraseña que ingresaste no coinciden con nuestros registros. Por favor, revisa e inténtalo de nuevo.',
-
 }

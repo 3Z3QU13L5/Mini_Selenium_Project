f9e6bfc8eec9bb8ef43819a14dc725e20541a422
Miguel Solano <miguel.abreu03@gmail.com>
2021-12-19 20:24:57 -0400

    Feat: Adding 2 step login.

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -17,6 +17,23 @@ module.exports = class login extends basepage {
         await this.click_on(page.login_button);
     }
 
+    async valid_login2steps (data, page) {
+        
+        // wait for a web element to be located
+        await this.wait_by_css(page.uid_input);
+        
+        // actions on web elements 1st Step
+        await this.type_into(page.uid_input, data.email);
+        await this.click_on(page.next_button);
+
+        // wait for a web element to be located
+        await this.wait_by_css(page.password_input);
+
+        // actions on web elements 2nd Step
+        await this.type_into(page.password_input, data.password);
+        await this.click_on(page.login_button);
+    }
+
     async invalid_login (data, page) {
         
         // wait for a web element to be located

7a5bb7aab9eb5ad37d934a6faf912ad5349a77d3
Miguel Solano <miguel.abreu03@gmail.com>
2021-12-26 12:29:45 -0400

    Feat: updating the screen file.

--- pages/screen.js
+++ pages/screen.js
@@ -1,5 +1,7 @@
 const {Builder, By, Key, until, WebDriverWait} = require("selenium-webdriver");
-var {assert} = require("chai")
+var {assert} = require("chai");
+const { Driver } = require("selenium-webdriver/chromium");
+const { url } = require("../selectors/facebook");
 
 module.exports = class basepage {
     constructor () {
@@ -48,8 +50,12 @@ module.exports = class basepage {
         global.driver = new Builder().forBrowser(browser).build();
     }
 
+    async set_newDriver (newDriver) {
+        global.driver = newDriver;
+    }
+
     async check_element (selector, text = '') {
-        
+
         let element = await driver.findElement(By.css(selector));
         let inText = await element.getText();
         let isPresent = await element.isDisplayed();
@@ -59,6 +65,32 @@ module.exports = class basepage {
             isCorrect = text.includes(inText);
         }
 
-        return {isPresent, isCorrect}
+        return {isPresent, isCorrect};
+    }
+
+    async get_driver () {
+        return driver;
+    }
+
+    async get_element (selector) {
+        let element = await driver.findElement(By.css(selector));
+        return element;
+    }
+
+    async create_selector (currentSelector, addittion) {
+        
+        let url = await driver.getCurrentUrl();
+        let selector = currentSelector;
+        
+        if(url.includes('facebook')) {
+            selector = selector.replace("TK", addittion)
+        };
+        
+        return selector
+    }
+
+    async get_url (){
+        let url = await driver.getCurrentUrl();
+        return url;
     }
 }
